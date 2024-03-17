9e2e1606f38d27da8d7ceb7737bff0b0bb3cfb77
Chengyuan <1136427613@qq.com>
2024-03-17 23:39:56 +0800

    feat: format & notes: can input 3D tensor to Linear

--- networks/mlp.py
+++ networks/mlp.py
@@ -1,15 +1,20 @@
 import torch
 import torch.nn as nn
+
 # import torch.nn.functional as F
 
+
+# input: (BACH_SIZE, LEN, 20)
+# Linear: (20, n) -> ReLU -> (n, 3)
 class MLP(nn.Module):
-  def __init__(self, feat_dim=3):
-    self.encoder = nn.Sequential(
-        nn.Linear(dim_in, dim_in),
-        nn.ReLU(inplace=True),
-        nn.Linear(dim_in, feat_dim)
-    )
-  def forward(self, x):
-    feat = self.encoder(x)
-    # feat = F.normalize(self.head(feat), dim=1)
-    return feat
+    def __init__(self, dim_in=20, feat_dim=3):
+        self.encoder = nn.Sequential(
+            nn.Linear(dim_in, dim_in),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim_in, feat_dim),
+        )
+
+    def forward(self, x):
+        feat = self.encoder(x)
+        # feat = F.normalize(self.head(feat), dim=1)
+        return feat
