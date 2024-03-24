761bc1bf1e9335a14ab92b0902b9f719b76f4c29
Chengyuan <1136427613@qq.com>
2024-03-24 21:16:41 +0800

    refactor & impl mlp

---
+++ src/dataset.py
@@ -0,0 +1,82 @@
+import os
+import pickle
+import torch
+from torch.utils.data import Dataset
+import torch.nn.functional as F
+
+    
+amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
+secondary_structure = 'CEH'
+
+class ProteinDataset(Dataset):
+      
+    # protein_seqs: list of protein sequences [seq, ssp]
+    # train: whether the dataset is for training
+    # split_len: when train = True, the split length of the sequence
+    def __init__(self, protein_seqs, train=True, split_len=250):  
+        self.seqs = [seq['seq'] for seq in protein_seqs]
+        self.ssps = [seq['ssp'] for seq in protein_seqs]
+        
+        if train:
+            split_seqs = []
+            split_ssps = []
+            for seq in self.seqs:
+                split_seqs.extend(self.split_seq(seq, split_len))
+            for ssp in self.ssps:
+                split_ssps.extend(self.split_seq(ssp, split_len))
+            seqs_tensor = [self.seq_to_onehot(seq, reqular_len=split_len).view(-1) for seq in split_seqs]
+            ssp_tensor = [self.seq_to_onehot(ssp, is_ssp=True, reqular_len=split_len).view(-1) for ssp in split_ssps]
+            self.seqs = seqs_tensor
+            self.ssps = ssp_tensor
+        
+            
+    def __len__(self):
+        return len(self.seqs)
+
+    def __getitem__(self, idx):
+        return self.seqs[idx], self.ssps[idx]
+    
+    @staticmethod
+    def load_data(seq_dir):
+        protein_seqs = []
+        data_source = os.listdir(seq_dir)
+        for file in data_source:
+            if file.endswith('.pkl'):
+                protein_seq = pickle.load(open(os.path.join(seq_dir, file), 'rb'))
+                protein_seqs.append(protein_seq)
+        return protein_seqs
+    
+    @staticmethod
+    def split_seq(seq, split_len = 250):
+        # seq: a string of amino acids/secondary structure
+        return [seq[i:i+split_len] for i in range(0, len(seq), split_len)]
+    
+    @staticmethod
+    def seq_to_onehot(seq, is_ssp = False, reqular_len = 250):
+        # note: len(seq) <= reqular_len
+        letters = secondary_structure if is_ssp else amino_acids
+        letter_to_index = {letter: index for index, letter in enumerate(letters)}
+        tensor = torch.zeros(reqular_len, len(letters))
+        for i, letter in enumerate(seq):
+                tensor[i, letter_to_index[letter]] = 1
+        return tensor
+    
+    @staticmethod
+    def onehot_to_sequence(one_hot, is_ssp=False):
+        letters = secondary_structure if is_ssp else amino_acids
+        letter_to_index = {letter: index for index, letter in enumerate(letters)}
+        # 使用torch.argmax找到每一行中值为1的元素的索引
+        indices = torch.argmax(one_hot, dim=1)
+        # 使用这些索引在index_to_aa字典中查找对应的氨基酸
+        sequence = ''.join([letters[index] for index in indices])
+        return sequence
+    
+    @staticmethod
+    def prob_to_onehot(prob):
+        # 找到最大概率的位置
+        max_prob_indices = torch.argmax(prob, dim=1)
+        # 生成one-hot向量
+        one_hot = F.one_hot(max_prob_indices, num_classes=prob.shape[-1])
+        return one_hot
+
+            
---
+++ src/mlp.py
@@ -0,0 +1,149 @@
+import torch
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader, SubsetRandomSampler
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from random import shuffle
+from dataset import ProteinDataset
+
+# import torch.nn.functional as F
+
+
+# input: (BACH_SIZE, LEN, 20)
+# Linear: (20, n) -> ReLU -> (n, 3)
+class MLP(nn.Module):
+    def __init__(
+        self,
+        split_len=20,
+        hidden=64,
+        epochs=100,
+        batch_size=32,
+        learning_rate=1e-4,
+    ):
+        super().__init__()
+        self.split_len = split_len
+        dim_in = split_len * 20
+        feat_dim = split_len * 3
+        self.fcs = nn.Sequential(
+            nn.Linear(dim_in, hidden),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden, feat_dim),
+        )
+        self.softmax = nn.Softmax(dim=-1)
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+
+    def forward(self, x):
+        h = self.fcs(x)
+        h = h.view(-1, self.split_len, 3)
+        x = self.softmax(h)
+        x = x.view(-1, self.split_len * 3)
+        return x
+
+    def save_model(self, save_file):
+        print("==> Saving...")
+        state = {
+            "model": self.state_dict(),
+            "epoch": self.epochs,
+        }
+        torch.save(state, save_file)
+        del state
+
+    def load_data(self, protein_seqs):
+
+        split = int(0.8 * len(protein_seqs))
+        shuffle(protein_seqs)
+
+        test_data = ProteinDataset(
+            protein_seqs[:split], train=True, split_len=self.split_len
+        )
+        valid_data = ProteinDataset(protein_seqs[split:], train=False)
+
+        self.train_loader = DataLoader(
+            test_data, batch_size=self.batch_size, shuffle=True
+        )
+        self.valid_loader = DataLoader(
+            valid_data, batch_size=self.batch_size, shuffle=True
+        )
+
+        
+    def train(self, verbose=False):
+        self = self.to(self.device)
+        self.loss_function = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+        self.loss_df = pd.DataFrame(columns=["epoch", "loss"])
+
+        for epoch in range(0, self.epochs):
+            if verbose:
+                print(f"Starting epoch {epoch+1}")
+
+            current_loss = 0.0
+
+            for i, data in enumerate(self.train_loader, 0):
+                inputs, targets = data[0].to(self.device), data[1].to(self.device)
+                self.optimizer.zero_grad()
+                outputs = self(inputs)
+                loss = self.loss_function(outputs, targets)
+                loss.backward()
+                self.optimizer.step()
+                current_loss += loss.item()
+
+            self.loss_df = self.loss_df._append(
+                {"epoch": epoch, "loss": current_loss / len(self.train_loader)},
+                ignore_index=True,
+            )
+        
+        # plt.plot(self.loss_df["epoch"], self.loss_df["loss"])
+
+    def save_res(self, save_file):
+        self.loss_df.to_csv(f"{save_file}_loss.csv", index=False)
+        self.save_model(save_file)
+        # plt.plot(self.loss_df["epoch"], self.loss_df["loss"])
+
+    def load_model(self, save_file):
+        saved_params = torch.load(save_file)
+        self.load_state_dict(saved_params["model"])
+        self = self.to(self.device)
+        epoch = saved_params["epoch"]
+        print(f"Loaded model: {save_file}")
+
+    def valid(self, verbose=False):
+        self.eval()    
+        with torch.no_grad():
+            q3s = []
+            for data in self.valid_loader:
+                inputs, targets = data[0], data[1]
+                for seq, ssp in zip(inputs, targets):
+                    
+                    split_seq = ProteinDataset.split_seq(seq, self.split_len)
+                    seq_tensors = [ProteinDataset.seq_to_onehot(seq, reqular_len=self.split_len).view(-1) for seq in split_seq]
+                    input_tensor = torch.stack(seq_tensors)
+                    input_tensor = input_tensor.to(self.device)
+                    # print(input_tensor.shape)
+                    output_tensor = self(input_tensor)
+                    # print(output_tensor.shape)
+                    
+                    one_hot = ProteinDataset.prob_to_onehot(output_tensor.view(-1, 3))
+                    # print(one_hot.shape)
+                    ssp_predicted = ProteinDataset.onehot_to_sequence(one_hot, is_ssp=True)
+                    if verbose:
+                        print(f"True_len: {len(ssp)}")
+                        print(f"Pred_len: {len(ssp_predicted)}")
+                        print(f"True: {ssp}")
+                        print(f"Pred: {ssp_predicted}")
+                        
+                    q3 = self.q3(ssp, ssp_predicted)
+                    q3s.append(q3)
+                    if verbose:
+                        print(f"Q3: {q3}")
+                        
+        return q3s
+                    
+    def q3(self, ssp_true, ssp_predicted):
+        q3 = sum([1 for i in range(len(ssp_true)) if ssp_true[i] == ssp_predicted[i]]) / len(ssp_true)
+        return q3
